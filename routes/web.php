5fd43b8ec14ead40c893d21cb5f5929d601108e2
Shane Parsons <shane@30ptdesign.com>
2017-04-25 16:23:51 -0400

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$app->get('/', function () use ($app) {
+
+    /**
+     * bug:
+     * type `()->json` (dont' copy and paste) after the word "response" (below) and watch the php task in the activity monitor
+     * the leak begins after the `->` is typed... no save neccessary
+     */
+
+    return response('Success', 200); 
+});
